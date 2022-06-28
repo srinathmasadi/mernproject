a4bd48cd738aa316c1781cd10fc61afb38aea0fc
Masadi Srinath <masadi.srianth@venturepact.com>
2022-06-28 16:28:33 +0530

    [Deployed]

--- moviewebsite/src/context.js
+++ moviewebsite/src/context.js
@@ -1,47 +1,23 @@
-import React, { useContext, useEffect, useState } from "react";
-const API_URL=`http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY}&s=titanic`
+import React, { useContext, useState } from "react";
+import useFetch from "./useFetch";
 
 const AppContext = React.createContext();
 
 
-const AppProvider = ({children})=>{
-    const [isLoading, setIsLoading] = useState(true);
-    const [movie, setMovie] = useState([]);
-    const[isError, setIsError] = useState({show:"false",msg:""})
-   const getMovies=async(url)=>{
-        try {
-            const res = await fetch(url);
-            const data = await res.json();
-            if(data.Response==="True"){
-                setIsLoading(false);
-                setMovie(data.Search);
-            } else{
-                setIsError({
-                    show:"true",
-                    msg:data.error
-                })
-            }
-            console.log(data);
-        } catch (error) {
-            console.log(error);
-        }
-    }
+// we are getting the children and that is app component in our case
+const AppProvider = ({ children }) => {
+  const [query, setQuery] = useState("hacker");
+  const { isLoading, isError, movie } = useFetch(`&s=${query}`);
 
-    useEffect(() => {
-        getMovies(API_URL);
-    }, [])
-    
-    return <AppContext.Provider value={
-        {
-            isLoading, isError, movie
-        }
-    }>
-            {children}
-        </AppContext.Provider>
+  return (
+    <AppContext.Provider value={{ query, movie, setQuery, isLoading, isError }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-const useGlobalContext = ()=>{
-    return useContext(AppContext);
-}
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
-export {AppContext, AppProvider,useGlobalContext}
+export { AppContext, AppProvider, useGlobalContext };
